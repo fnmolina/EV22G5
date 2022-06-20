7ab4e9ed30d2630c337a391b8d9c8ddbd43dfa04
Carlos <31170466+CarlosAngelChen@users.noreply.github.com>
2022-06-20 19:19:00 -0300

    add mux and muxk

--- modules/mux_k.v
+++ modules/mux_k.v
@@ -1,14 +1,14 @@
-module mux (
+module mux_k (
     input sel,
     input [15:0] D0,
-    input [15:0] D1,
+    input [15:0] K,
     output reg [15:0] Q
 );
-    always @(D0 or D1 or sel) begin
+    always @(D0 or K or sel) begin
         if (sel) begin
-            Q <= D1;
+            Q <= K & 16'h01ff;
         end else begin
             Q <= D0;
         end
-    end  
+    end
 endmodule
